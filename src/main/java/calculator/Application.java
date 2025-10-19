8fc0b0c6d4b813b2467db35f27eb2bcf67a60509
강인화 <rkddlsghk1998@gmail.com>
2025-10-19 19:56:00 +0900

    feat: 숫자 합계 반환 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input;
+        String[] tokens;
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        input = Console.readLine();
+        tokens = input.split("[,:]");
+
+        if (input.isEmpty()) {
+            System.out.println("결과 : " + 0);
+            return;
+        }
+
+        Arrays.stream(tokens)
+                .map(Integer::parseInt)
+                .reduce(Integer::sum)
+                .ifPresent(result -> System.out.println("결과 : " + result));
     }
 }
