19b9c1f49845c378e4be7709d7798d6a26c8c917
강인화 <rkddlsghk1998@gmail.com>
2025-10-19 21:39:10 +0900

    feat: 예외 처리 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -12,14 +12,27 @@ public class Application {
         String body;
         final String commandString = "//";
         final String delimitersString = "\\\\n";
+        final int customDelimiterLength = 1;
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         input = Console.readLine();
 
         if (input.startsWith(commandString)) {
             String[] tempTokens = input.split(delimitersString, 2);
+            if (tempTokens.length != 2) {
+                throw new IllegalArgumentException("올바르지 않은 입력 형식입니다.");
+            }
             String header = tempTokens[0];
+            if (header.length() != commandString.length() + customDelimiterLength) {
+                throw new IllegalArgumentException("커스텀 구분자는 유일합니다.");
+            }
             char customDelimiter = header.charAt(commandString.length());
+            if (delimiters.contains(String.valueOf(customDelimiter))) {
+                throw new IllegalArgumentException("커스텀 구분자는 기본 구분자일 수 없습니다.");
+            }
+            if (Character.isDigit(customDelimiter)) {
+                throw new IllegalArgumentException("커스텀 구분자는 숫자일 수 없습니다.");
+            }
             delimiters += customDelimiter;
             body = tempTokens[1];
         } else {
@@ -33,7 +46,17 @@ public class Application {
 
         tokens = body.split("[" + delimiters + "]");
         Arrays.stream(tokens)
-                .map(Integer::parseInt)
+                .map(token -> {
+                    try {
+                        int parsed = Integer.parseInt(token);
+                        if (parsed <= 0) {
+                            throw new IllegalArgumentException("양수만 가능합니다.");
+                        }
+                        return parsed;
+                    } catch (NumberFormatException e) {
+                        throw new IllegalArgumentException("");
+                    }
+                })
                 .reduce(Integer::sum)
                 .ifPresent(result -> System.out.println("결과 : " + result));
     }
