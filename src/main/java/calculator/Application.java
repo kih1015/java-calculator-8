13d367dbf7bc78bf45e2bc8483f7000468aee192
강인화 <rkddlsghk1998@gmail.com>
2025-10-19 20:35:03 +0900

    feat: 커스텀 구분자 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,16 +8,30 @@ public class Application {
     public static void main(String[] args) {
         String input;
         String[] tokens;
+        String delimiters = ",:";
+        String body;
+        final String commandString = "//";
+        final String delimitersString = "\\\\n";
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         input = Console.readLine();
-        tokens = input.split("[,:]");
 
-        if (input.isEmpty()) {
+        if (input.startsWith(commandString)) {
+            String[] tempTokens = input.split(delimitersString, 2);
+            String header = tempTokens[0];
+            char customDelimiter = header.charAt(commandString.length());
+            delimiters += customDelimiter;
+            body = tempTokens[1];
+        } else {
+            body = input;
+        }
+
+        if (body.isEmpty()) {
             System.out.println("결과 : " + 0);
             return;
         }
 
+        tokens = body.split("[" + delimiters + "]");
         Arrays.stream(tokens)
                 .map(Integer::parseInt)
                 .reduce(Integer::sum)
