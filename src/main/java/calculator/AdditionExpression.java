e3a6cdb84f57f266bc08b2592d514cbbce9520df
강인화 <rkddlsghk1998@gmail.com>
2025-10-20 02:45:09 +0900

    refactor: 파서 통합

--- src/main/java/calculator/AdditionExpression.java
+++ src/main/java/calculator/AdditionExpression.java
@@ -2,18 +2,46 @@ package calculator;
 
 public class AdditionExpression {
 
+    private static final String HEADER_START_STRING = "//";
+    private static final String HEADER_END_STRING = "\\\\n";
+
     private final DelimiterList delimiterList;
     private final OperandList operandList;
 
     public AdditionExpression(String rawCommand, Character[] defaultDelimiters) {
         this.delimiterList = new DelimiterList(defaultDelimiters);
-        Parser parser = new Parser(rawCommand);
-        parser.getCustomDelimiter()
-                .ifPresent(delimiterList::add);
-        this.operandList = new OperandList(parser.getExpression(), delimiterList.getDelimiterList());
+        String expression = parse(rawCommand);
+        this.operandList = new OperandList(expression, delimiterList.getDelimiterList());
     }
 
     public int sum() {
         return operandList.sum();
+    }
+
+    private String parse(String rawCommand) {
+        if (!rawCommand.startsWith(HEADER_START_STRING)) {
+            return rawCommand;
+        }
+
+        String[] tempTokens = rawCommand.split(HEADER_END_STRING, 2);
+        validateInputString(tempTokens);
+
+        String header = tempTokens[0];
+        delimiterList.add(extractCustomDelimiter(header));
+        return tempTokens[1];
+    }
+
+    private void validateInputString(String[] tempTokens) {
+        if (tempTokens.length != 2) {
+            throw new IllegalArgumentException("올바르지 않은 입력 형식입니다.");
+        }
+    }
+
+    private char extractCustomDelimiter(String header) {
+        int customDelimiterLength = 1;
+        if (header.length() != HEADER_START_STRING.length() + customDelimiterLength) {
+            throw new IllegalArgumentException("커스텀 구분자는 유일합니다.");
+        }
+        return header.charAt(HEADER_START_STRING.length());
     }
 }
