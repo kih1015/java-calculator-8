e2edee2c7979d1b11f3a38b0c4ac23f0188e5a21
강인화 <rkddlsghk1998@gmail.com>
2025-10-20 21:46:43 +0900

    refactor: 파싱 로직 개선

--- src/main/java/calculator/AdditionExpression.java
+++ src/main/java/calculator/AdditionExpression.java
@@ -5,14 +5,20 @@ import java.util.List;
 public class AdditionExpression {
 
     private static final String HEADER_START_STRING = "//";
-    private static final String HEADER_END_STRING = "\\\\n";
+    private static final String HEADER_END_STRING = "\\n";
 
     private final DelimiterList delimiterList;
     private final OperandList operandList;
 
     public AdditionExpression(String rawCommand, List<Delimiter> defaultDelimiters) {
         this.delimiterList = new DelimiterList(defaultDelimiters);
-        String expression = parse(rawCommand);
+
+        if (hasCustomDelimiter(rawCommand)) {
+            char customDelimiter = extractCustomDelimiter(rawCommand);
+            this.delimiterList.add(customDelimiter);
+        }
+
+        String expression = extractExpression(rawCommand);
         this.operandList = new OperandList(expression, delimiterList.toRegex());
     }
 
@@ -20,30 +26,19 @@ public class AdditionExpression {
         return operandList.sum();
     }
 
-    private String parse(String rawCommand) {
-        if (!rawCommand.startsWith(HEADER_START_STRING)) {
+    private boolean hasCustomDelimiter(String rawCommand) {
+        String header = rawCommand.substring(0, 5);
+        return header.startsWith(HEADER_START_STRING) && header.endsWith(HEADER_END_STRING);
+    }
+
+    private char extractCustomDelimiter(String rawCommand) {
+        return rawCommand.charAt(2);
+    }
+
+    private String extractExpression(String rawCommand) {
+        if (!hasCustomDelimiter(rawCommand)) {
             return rawCommand;
         }
-
-        String[] tempTokens = rawCommand.split(HEADER_END_STRING, 2);
-        validateInputString(tempTokens);
-
-        String header = tempTokens[0];
-        delimiterList.add(extractCustomDelimiter(header));
-        return tempTokens[1];
-    }
-
-    private void validateInputString(String[] tempTokens) {
-        if (tempTokens.length != 2) {
-            throw new IllegalArgumentException("올바르지 않은 입력 형식입니다.");
-        }
-    }
-
-    private char extractCustomDelimiter(String header) {
-        int customDelimiterLength = 1;
-        if (header.length() != HEADER_START_STRING.length() + customDelimiterLength) {
-            throw new IllegalArgumentException("커스텀 구분자는 유일합니다.");
-        }
-        return header.charAt(HEADER_START_STRING.length());
+        return rawCommand.substring(5);
     }
 }
